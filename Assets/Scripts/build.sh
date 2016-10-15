14ad05c4b03402982fb2754affc9c7e926a19083
Виталий Буланенков <vitaly.bulanenkov@gmail.com>
2016-10-15 13:31:13 +0300

    fixes

--- Assets/Scripts/build.sh
+++ Assets/Scripts/build.sh
@@ -15,10 +15,10 @@ echo "Attempting to build $project for Windows"
   -projectPath $(pwd) \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
-rm -r "$(pwd)/Build/windows/DLL"
+rm -r "$(pwd)/Build/windows/DLLs"
 rm -r "$(pwd)/Build/windows/Code"
 rm -r "$(pwd)/Build/windows/Mods"
-cp -r "$(pwd)/DLL" "$(pwd)/Build/windows"
+cp -r "$(pwd)/DLLs" "$(pwd)/Build/windows"
 cp -r "$(pwd)/Code" "$(pwd)/Build/windows"
 cp -r "$(pwd)/Mods" "$(pwd)/Build/windows"
 zip -r windows_build.zip "$(pwd)/Build/windows"
@@ -32,10 +32,10 @@ echo "Attempting to build $project for OS X"
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
-rm -r "$(pwd)/Build/osx/DLL"
+rm -r "$(pwd)/Build/osx/DLLs"
 rm -r "$(pwd)/Build/osx/Code"
 rm -r "$(pwd)/Build/osx/Mods"
-cp -r "$(pwd)/DLL" "$(pwd)/Build/osx"
+cp -r "$(pwd)/DLLs" "$(pwd)/Build/osx"
 cp -r "$(pwd)/Code" "$(pwd)/Build/osx"
 cp -r "$(pwd)/Mods" "$(pwd)/Build/osx"
 zip -r osx_build.zip "$(pwd)/Build/osx"
@@ -50,10 +50,10 @@ echo "Attempting to build $project for Linux"
   -projectPath $(pwd) \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
-rm -r "$(pwd)/Build/linux/DLL"
+rm -r "$(pwd)/Build/linux/DLLs"
 rm -r "$(pwd)/Build/linux/Code"
 rm -r "$(pwd)/Build/linux/Mods"
-cp -r "$(pwd)/DLL" "$(pwd)/Build/linux"
+cp -r "$(pwd)/DLLs" "$(pwd)/Build/linux"
 cp -r "$(pwd)/Code" "$(pwd)/Build/linux"
 cp -r "$(pwd)/Mods" "$(pwd)/Build/linux"
 zip -r linux_build.zip "$(pwd)/Build/linux"
