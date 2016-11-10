5e283d1f0a10d99fbecae978f0a761b548bc06db
Виталий Буланенков <vitaly.bulanenkov@gmail.com>
2016-11-10 04:40:46 +0300

    build function update

--- Assets/ItchDeploy.cs
+++ Assets/ItchDeploy.cs
@@ -13,28 +13,37 @@ namespace Assets
         static string ChannelName = "windows";
         public static void OnProjectBuilt(string pathToPlayer)
         {
-            File.Delete("/butler_creds");
-            File.Delete("/butler.exe");
-            //File.Delete
-            File.Move(pathToPlayer + "/ExperimentalProject_Data/butler_creds", "/butler_creds");
-            File.Move(pathToPlayer + "ExperimentalProject_Data/StreamingAssets/butler.exe", "/butler.exe");
-
-            string args = String.Format(" -i {0} push {1} {2}:{3}",
+            try
+            {
+                File.Delete("/butler_creds");
+                File.Delete("/butler.exe");
+            
+                File.Move(pathToPlayer + "/ExperimentalProject_Data/butler_creds", "/butler_creds");
+                File.Move(pathToPlayer + "ExperimentalProject_Data/StreamingAssets/butler.exe", "/butler.exe");
+      
+                string args = String.Format(" -i {0} push {1} {2}:{3}",
                 "/butler_creds",
                 "ExperimentalProject.zip",
                 AccountName,
                 ChannelName                                
                 );
-            ZipFile file = new ZipFile();
-            file.AddDirectory(pathToPlayer);
-            file.Save("ExperimentalProject.zip");
+                ZipFile file = new ZipFile();
+                file.AddDirectory(pathToPlayer);
+                file.Save("ExperimentalProject.zip");
 
-            System.Diagnostics.Process uploadProc = new System.Diagnostics.Process();
-            uploadProc.StartInfo.FileName = "/butler.exe";
-            uploadProc.StartInfo.Arguments = args;
-            uploadProc.StartInfo.CreateNoWindow = false;
-            uploadProc.StartInfo.UseShellExecute = false;
-            uploadProc.Start();
+                System.Diagnostics.Process uploadProc = new System.Diagnostics.Process();
+                uploadProc.StartInfo.FileName = "/butler.exe";
+                uploadProc.StartInfo.Arguments = args;
+                uploadProc.StartInfo.CreateNoWindow = false;
+                uploadProc.StartInfo.UseShellExecute = false;
+                uploadProc.Start();
+
+                uploadProc.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogWarning(e);
+            }
         }
 
         
