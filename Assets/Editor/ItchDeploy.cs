90836ef88c62982ce15fca918cc9a6c0e182fffd
Виталий Буланенков <vitaly.bulanenkov@gmail.com>
2016-11-11 00:25:14 +0300

    itch butler mac app instead of win exe

--- Assets/Editor/ItchDeploy.cs
+++ Assets/Editor/ItchDeploy.cs
@@ -20,9 +20,9 @@ static class ItchDeploy
             var pathToEditor = pathToPlayer.Substring(0, pathToPlayer.LastIndexOf('/') +1);
 
             File.Delete(pathToEditor + "butler_creds");
-            File.Delete(pathToEditor + "butler.exe");
+            File.Delete(pathToEditor + "butler");
             File.Move(pathToPlayer + "ExperimentalProject_Data/StreamingAssets/butler_creds", pathToEditor + "butler_creds");
-            File.Move(pathToPlayer + "ExperimentalProject_Data/StreamingAssets/butler.exe", pathToEditor + "butler.exe");
+            File.Move(pathToPlayer + "ExperimentalProject_Data/StreamingAssets/butler", pathToEditor + "butler");
       
             string args = String.Format(" -i {0} push {1} {2}:{3}",
             pathToEditor + "butler_creds",
@@ -35,7 +35,7 @@ static class ItchDeploy
             file.Save(pathToEditor + "ExperimentalProject.zip");
 
             System.Diagnostics.Process uploadProc = new System.Diagnostics.Process();
-            uploadProc.StartInfo.FileName = pathToEditor + "butler.exe";
+            uploadProc.StartInfo.FileName = pathToEditor + "butler";
             uploadProc.StartInfo.Arguments = args;
             uploadProc.StartInfo.CreateNoWindow = false;
             uploadProc.StartInfo.UseShellExecute = false;
